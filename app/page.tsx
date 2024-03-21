e708d56f7458833bb1592e14121cbaa636b0f973
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-03-21 14:39:03 +0000

    sets up inital page structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import Link from "next/link";
+import ReactLogo from "./assests/Logo.svg";
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <main className="h-full w-full relative">
+      <div className="bg-white h-full w-full">Grid Layer Component</div>
+      <div className="absolute h-full w-full top-0">Chat overlay</div>
+    </main>
+  );
 }
