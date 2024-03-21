e2c4d4e43d714fc1db730150ca3e85f53c57908c
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-03-21 20:03:44 +0000

    adds Cars entities interface

---
+++ app/entities/Cars.ts
@@ -0,0 +1,12 @@
+export interface Cars {
+  Brand: string,
+  Model: string,
+  Price: number,
+  Acceleration: number,
+  Horsepower: number,
+  Type: string,
+  Size: string,
+  Category: string,
+  Engine: string,
+  Car_Name: string
+}
