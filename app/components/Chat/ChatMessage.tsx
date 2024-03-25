6b55e2f8965be95822cdf53dc918e9c8dab55ca3
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-03-25 14:54:41 +0000

    adds chat avatar

--- app/components/Chat/ChatMessage.tsx
+++ app/components/Chat/ChatMessage.tsx
@@ -1,9 +1,21 @@
 import React from "react";
+import Image from "next/image";
 import Message from "../../entities/Message";
+import chatAvatar from "@/public/chatAvatar.png";
+import { RxAvatar } from "react-icons/rx";
 
 const ChatMessage = ({ id, role, content }: Message) => {
   return (
     <div className={`chat ${role === "user" ? "chat-end" : "chat-start"}`}>
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full shadow">
+          {role !== "user" ? (
+            <Image src={chatAvatar} alt={role} className="!object-scale-down" />
+          ) : (
+            <RxAvatar className="h-10 w-10" />
+          )}
+        </div>
+      </div>
       <div className="chat-header">{role}</div>
       <div
         className={`chat-bubble ${
