d933c72598d7b93d358a0b8fc403c208988495c2
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-03-27 14:07:43 +0000

    adds in fetch to BE for data

--- api/index.py
+++ api/index.py
@@ -1,5 +1,6 @@
 from typing import List, Optional
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, parse_obj_as
 import logging
 import pandas as pd
@@ -30,6 +31,18 @@ class CarResponse(BaseModel):
     cars: List[Car]
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",  # Allow frontend origin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # List of allowed origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/api/cars")
 def hello_world():
