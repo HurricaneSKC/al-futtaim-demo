a5ebf719f90fab4c06467216295cb26d425424ee
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-04-12 15:18:57 +0100

    mocks random images to placeholders and adds model to car basic info

--- app/components/CarGrid/CarGridElement/CarBasicInfo.tsx
+++ app/components/CarGrid/CarGridElement/CarBasicInfo.tsx
@@ -7,9 +7,11 @@ export default function CarBasicInfo({
   brand,
   size,
   className,
+  model,
 }: {
   brand: string;
   size: string;
+  model: string;
   className?: string;
 }) {
   const { level } = useGridContext();
@@ -17,10 +19,14 @@ export default function CarBasicInfo({
   return (
     <div className={cn("flex flex-col justify-start items-start", className)}>
       {(level === "1" || level === "2") && (
-        <h1 className="text-[6px] font-semibold">{brand}</h1>
+        <h1 className="text-[6px] font-semibold">
+          {brand}-{model}
+        </h1>
       )}
       {level === "2" && (
-        <span className="text-[#8D8D8D] text-[4px] animate-slideIn">{size}</span>
+        <span className="text-[#8D8D8D] text-[4px] animate-slideIn">
+          {size}
+        </span>
       )}
     </div>
   );
