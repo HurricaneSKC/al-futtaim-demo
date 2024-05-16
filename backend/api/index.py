082c4bb62d0a102edd67b528c83ec7b33830eb72
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-05-16 10:29:38 +0100

    folder restructure, decouple FE and BE code

--- backend/api/index.py
+++ backend/api/index.py
@@ -2,10 +2,11 @@ from typing import List, Optional
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel, parse_obj_as
-import time;
+import time
 import logging
 import pandas as pd
-from .filter_cars_using_prompt_conversation import generate_response_and_update_messages, load_and_preprocess_data
+from api.filter_cars_using_prompt_conversation import generate_response_and_update_messages, load_and_preprocess_data
+
 
 class Car(BaseModel):
     Brand: str
@@ -19,17 +20,21 @@ class Car(BaseModel):
     Acceleration: float
     Horsepower: float
 
+
 class Message(BaseModel):
     role: str
     content: str
+
 
 class CarPrompt(BaseModel):
     messages: List[Message]
     cars: Optional[List[Car]] = []
 
+
 class CarResponse(BaseModel):
     messages: List[Message]
     cars: List[Car]
+
 
 app = FastAPI()
 
@@ -45,9 +50,11 @@ app.add_middleware(
     allow_headers=["*"],  # Allow all headers
 )
 
+
 @app.get("/api/cars")
 def hello_world():
     return {"message": "Hello World"}
+
 
 @app.post("/api/cars", response_model=CarResponse)
 async def get_cars(data: CarPrompt):
@@ -56,8 +63,7 @@ async def get_cars(data: CarPrompt):
     logger.info(f'Received data: {data}')
 
     if not data.cars:
-        df = load_and_preprocess_data(path= "api/aggregated_data.xlsx")
-
+        df = load_and_preprocess_data(path="api/aggregated_data.xlsx")
 
     else:
         logger.info(f' else ')
@@ -67,25 +73,23 @@ async def get_cars(data: CarPrompt):
         cars_dict = [car.__dict__ for car in data.cars]
         df = pd.DataFrame(cars_dict)
 
-
     messages_dic = [messages.__dict__ for messages in data.messages]
     # logger.info(f'messages_dic: {messages_dic}')
     # logger.info(f'data.messages: {data.messages}')
 
-
     # logger.info(f'df columns : {df.columns}')
     # logger.info(f'df  : {df.shape}')
 
-
     logger.info(f'before  generate_response_and_update_messages')
-    
+
     start = time.time()
 
-    output_df, messages = generate_response_and_update_messages(messages_dic, df)
+    output_df, messages = generate_response_and_update_messages(
+        messages_dic, df)
 
     logger.info(f'after  generate_response_and_update_messages')
     end = time.time()
-    
+
     print(end - start)
 
     # logger.info(f'messages : {messages}')
