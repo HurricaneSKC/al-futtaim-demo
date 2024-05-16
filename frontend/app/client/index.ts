86036868477362922382b3d8b5c87cedceba8582
Sanjay Chopra <sanjaykchopra@gmail.com>
2024-05-16 15:03:45 +0100

    updates endpoint for prod

--- frontend/app/client/index.ts
+++ frontend/app/client/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://al-futtaim-demo-u6m7umbyta-zf.a.run.app/api",
 });
 
 export default instance;
